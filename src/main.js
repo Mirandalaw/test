858571866d8c4d36a6bbd6ad64ba6a2d92487558
JeongPark <shrup5@naver.com>
2022-03-20 05:29:39 +0900

    Day2

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const app = express();
-const PORT =5000;
-app.get('/',(req,res)=>{
-    res.send('complete');
-})
-app.listen(PORT,()=>{
-    console.log(`The Express Server is Listening at port ${PORT}`)
-})
+const PORT = 5000;
+app.get('/', (req, res) => {
+  res.send('test');
+});
+app.listen(PORT, () => {
+  console.log(`The Express Server is Listening at port ${PORT}`);
+});
