cef6021e3d9373917456377d50581cdcf4deeb7b
JeongPark <shrup5@naver.com>
2022-04-14 23:49:43 +0900

    models/user

--- src/models/user.js
+++ src/models/user.js
@@ -1,53 +1,28 @@
-const { json } = require('express/lib/response');
 const mongodb = require('../utils/mongodbModule/mongoClient');
 
-module.exports = {
-  /**
-   * signUp user
-   *
-   * @param {Object} jsonObj
-   * @returns
-   */
-  signUp: async (jsonObj) => {
-    var indexOptions = {
-      sort: ['index', 'desc'],
-    };
+const signUp = async (jsonObj) => {
+  var indexOptions = {
+    sort: ['index', 'desc'],
+  };
+  var queryOptions = {
+    memberId: jsonObj.memberId,
+  };
 
-    var queryOptions = {
-      memberId: jsonObj.memberId,
-    };
+  let result = new Object();
 
-    let result = new Object();
-
-    await mongodb
-      .mongoSelectOne('users', {}, indexOptions)
-      .then((selectResult) => {
-        try {
-          jsonObj.index = selectResult.index;
-          jsonObj.index++;
-        } catch (err) {
-          jsonObj.index = 1;
-        }
-      })
-      .catch((err) => {
-        result.respose = 'Failed';
-        result.error = err;
-      });
-
-    await mongodb
-      .mongoSelectOne('users', queryOptions, {})
-      .then(function (selectResult) {
-        if (selectResult.length != 0) {
-          result.response = 'FAILED';
-        } else {
-          mongodb.mongoInsertOne('member', jsonObj);
-          result.response = 'SUC';
-        }
-      })
-      .catch(function (err) {
-        //mongoDB 에러시
-        result.response = 'FAILED';
-        result.error = err;
-      });
-  },
+  await mongodb
+    .mongoSelectOne('member', queryOptions, {})
+    .then((selectResult) => {
+      if (selectResult.lenght != 0) {
+        result.response = 'Failed';
+      } else {
+        mongodb.mongodbInsertOne('member', jsonObj);
+        result.response = 'Suc';
+      }
+    })
+    .catch((err) => {
+      result.response = 'Failed';
+      result.error = err;
+    });
+  return result;
 };
